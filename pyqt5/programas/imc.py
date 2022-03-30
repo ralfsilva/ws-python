022fd0a36269afc99130b256c272022bbe644df9
Ralf Velane <ralf.velani@gmail.com>
2022-03-30 20:19:50 -0300

    Último estágio Projeto IMC

---
+++ pyqt5/programas/imc.py
@@ -0,0 +1,44 @@
+import sys
+import math
+from PyQt5.QtWidgets import * 
+from PyQt5 import QtCore
+from PyQt5.QtGui import * 
+from PyQt5 import uic, QtWidgets
+
+def indice():
+    grau = 0
+
+    altura = float(massa.lineEdit_altura.text())
+    peso = float(massa.lineEdit_peso.text())
+
+    calculo_imc = (peso / math.pow(altura, 2))
+
+    if calculo_imc < 18.5:
+        massa.widget_magreza.setStyleSheet('background-color: #ebfdea;')        
+    elif calculo_imc >= 18.5 and calculo_imc < 25.0:
+        massa.widget_normal.setStyleSheet('background-color: #ebfdea;')
+    elif calculo_imc >= 25.0 and calculo_imc < 30.0:
+        grau = 1
+        massa.widget_sobrepeso.setStyleSheet('background-color: #ebfdea;')
+    elif calculo_imc >= 30.0 and calculo_imc < 40.0:
+        grau = 2
+        massa.widget_obesidade.setStyleSheet('background-color: #ebfdea;')
+    elif calculo_imc >= 40.0:
+        massa.widget_obesidade_grave.setStyleSheet('background-color: #ebfdea;')
+
+    calculo_imc = round(calculo_imc, 2)
+    calc = str(calculo_imc)
+    massa.label_seuimc.setText("SEU IMC: \n" + calc)
+    massa.pb_limpar.clicked.connect(massa.label_seuimc.clear)
+    
+    #massa.widget_sobrepeso.setStyleSheet('background-color: transparent; border-bottom: 3px solid #e7eaee;')
+    
+    #if massa.pb_limpar.clicked and grau == 1:
+    #    massa.widget_sobrepeso.setStyleSheet('background-color: transparent; border-bottom: #e7eaee;')
+
+app = QtWidgets.QApplication([])
+massa = uic.loadUi("gui_imc.ui")
+massa.pb_calcular.clicked.connect(indice)
+massa.pb_limpar.clicked.connect(indice)
+massa.show()
+app.exec()
